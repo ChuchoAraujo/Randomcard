8391821704cbc4db8f64125c05d81d6cccfc6dd5
ChuchoAraujo <jeseazuara@gmail.com>
2022-10-06 14:55:15 +0000

    Primer commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,30 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  //STEP 1 -CREAR CONSTANTES//
+  const palos = ["💗", "♠️", "🔸", "♣️"];
+  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+  // STEP 2- CREAR FUNCION //
+  const getRandom = array => {
+    const numeroAleatorio = Math.floor(Math.random() * array.length);
+    const elementoAleatorio = array[numeroAleatorio];
+    return elementoAleatorio;
+  };
+
+  // (ARROY FUNCTION =>)
+  // const getRandom2 = array => array[Math.floor(Math.random() * array.length)];
+
+  //INVOCACION FUNCION//
+  getRandom(palos);
+  getRandom(numbers);
+
+  //MANIPULAR DOM //
+  const h1Palo = document.querySelector("#paloAleatorio");
+  const h1Numero = document.querySelector("#numeroAleatorio");
+  const h1Palo2 = document.querySelector("#paloAleatorio2");
+
+  const duplicarPalo = (h1Palo.innerHTML = getRandom(palos));
+  h1Numero.innerHTML = getRandom(numbers);
+  h1Palo2.innerHTML = duplicarPalo;
 };
